af2c6c49f2853b1d37c7fcdda85095353cd3c9bd
AbramJenkingarten <id1er@yandex.ru>
2020-07-09 15:06:00 +0300

    Update 00tpvd.pl

--- 00tpvd.pl
+++ 00tpvd.pl
@@ -11,8 +11,8 @@
 # Price - это цена размещенной заявки, DealPrice - это цена по которой заявка исполнена
 use strict;
 
-my $srcdir = "/venv/data/QSH.Si-12.19";
-my $dstdir = "/venv/data/Si19X";
+my $srcdir = "/venv/data/Si-6.20";
+my $dstdir = "/venv/data/Si206";
 
 my $str ="";
 my $file="";
@@ -25,6 +25,7 @@ while(defined($file=<FHLS>))
   chomp($file);
   my @f2=split(/\./,$file);
   print "$file => $f2[2].$f2[5]\n";
+  ### Первый фильтр: оставляем сделки и убираем несистемные сделки
   system("grep Fill $srcdir/$file | grep -v NonSystem >/tmp/fltr1");
 
   open(FHI,"/tmp/fltr1");
@@ -33,38 +34,46 @@ while(defined($file=<FHLS>))
   while(defined($str=<FHI>))
     {
     my @a=split(/;/,$str);
-    # if DealId is 0
+    # Фильтр сделок: если поле DealId равно 0, то отбрасываем строку
     if($a[6]==0){next;}
 
-    # Select time from ExchTime.
+    # Фильтр дат: Received и ExchTime должны быть в один день
+    my @d0=split(/ /,$a[0]);
+    my @d1=split(/ /,$a[1]);
+    if ($d0[0] ne $d1[0]){next;}
+
+    # Конвертируем время
     my @t1=split(/ /,$a[1]);
     my @t2=split(/\./,$t1[1]);
     my @t3=split(/\:/,$t2[0]);
     my $t4=$t3[0]*60*60+$t3[1]*60+$t3[2]-36000;
+
+    # Из флагов выбираем направление сделки
     my @flags=split(/\,/,$a[9]);
     my $dir;
     if($flags[1] eq " Sell")
       {$dir="s";}
-    else
+    if($flags[1] eq " Buy")
       {$dir="b";}
 
     # Время 6DealId 2OrderId 7DealPrice 4Amount 9Flags
-    print FHO "$t4\t$a[6]\t$a[2]\t$a[7]\t$a[4]\t$dir\n";
+    print FHO "$t4;$a[6];$a[2];$a[7];$a[4];$dir\n";
     }
   close(FHI);
   close(FHO);
 
+  #
   open(FHI,"/tmp/fltr2");
   open(FHO,">$dstdir/$f2[2].$f2[5]");
 
-  my $str1="0   0       0       0       0       0";
+  my $str1=<FHI>;
   my $str2="";
   my $dir="";
   while(defined($str2=<FHI>))
     {
     chomp($str1); chomp($str2);
-    my @a1=split(/\t/,$str1);
-    my @a2=split(/\t/,$str2);
+    my @a1=split(/;/,$str1);
+    my @a2=split(/;/,$str2);
     if($a1[1] != $a2[1])
         {$str1=$str2;next;}
     if($a1[2] < $a2[2])
